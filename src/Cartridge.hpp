70a56be0ffd63a765283c2344bff72be9bed631b
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-02-22 23:57:44 +0000

    added Catridge class

---
+++ src/Cartridge.hpp
@@ -0,0 +1,17 @@
+#ifndef CARTRIDGE_HPP
+#define CARTRIDGE_HPP
+#include <cstdint>
+
+namespace bw {
+
+    using Byte = std::uint8_t;
+    using Address = std::uint16_t;
+
+    class Cartridge{
+
+    };
+
+
+};
+
+#endif
