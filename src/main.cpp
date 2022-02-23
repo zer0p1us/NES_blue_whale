db04ac388e2571dbfe50f8ad99989801cd4634f0
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-02-23 00:59:33 +0000

    added ROM detection

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,22 @@ int main(int argc, char const *argv[]) {
     sf::RenderWindow bw_window(sf::VideoMode(viewport_width, viewport_height), "Blue Whale");
     bw_window.setSize(sf::Vector2u(viewport_width, viewport_height));
 
+    std::vector<bw::Byte> rom_data;
     if (!argc) {
-         std::cout << "no arguments received" << '\n';
+        std::cout << "no arguments received" << '\n';
+        return 0;
     } else {
-        std::vector<bw::Byte> rom_data = read_rom(argv[0]);
+        rom_data = read_rom(argv[1]);
         }
+
+    if (rom_data[3] == 0x1A){
+        std::cout << "iNES format rom detected, loading rom..." << '\n';
+        std::cout << "there are " << int(rom_data[4]) << " backs of 16KB of PRG-ROM"  << '\n';
+        std::cout << "there are " << int(rom_data[5]) << " backs of 8KB of VROM"  << '\n';
+
+    }else{
+        std::cout << "this format is not supported, this emulator only supprot iNES roms" << '\n';
+    }
 
     while (bw_window.isOpen()){
         sf::Event event;
