a57613a4382b07c8dade172c901d3ee152ebb5f5
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-03-12 00:25:45 +0000

    added ROM class

---
+++ src/core/ROM.cpp
@@ -0,0 +1,24 @@
+#include "ROM.hpp"
+#include "../util/typedefs.hpp"
+#include <fstream>
+#include <vector>
+
+ROM::ROM(){
+
+}
+
+void ROM::read_rom(const char* rom_name){
+    // open file
+    std::streampos rom_size;
+    std::ifstream rom(rom_name, std::ios::binary);
+
+    // get the size
+    rom.seekg(0, std::ios::end);
+    rom_size = rom.tellg();
+    rom.seekg(0, std::ios::beg);
+
+    // read data
+    std::vector<Byte> rom_data(rom_size);
+    rom.read((char*) &rom_data[0], rom_size);
+    ROM::rom_content = rom_data;
+}
