b8c44f696b1f49322efdbd4e75ec9d3b4a685baa
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-03-22 10:39:52 +0000

    renamed read to prg_read

--- src/core/NROM.cpp
+++ src/core/NROM.cpp
@@ -1,7 +1,9 @@
 #include "NROM.hpp"
 
+#include <iostream>
 
-uint8_t NROM::read(uint16_t addr){
+
+uint8_t NROM::prg_read(uint16_t addr){
     if (addr < 0x8000) {
         return 0;
     }
--- src/core/NROM.hpp
+++ src/core/NROM.hpp
@@ -2,14 +2,15 @@
 #define NROM_HEADER
 
 #include "Mapper.hpp"
+#include <stdint.h>
 
 class NROM : public Mapper{
 
     public:
         NROM(std::vector<uint8_t> &prg_rom, std::vector<uint8_t> &chr_rom, int mirroring) : Mapper(prg_rom, chr_rom, mirroring) { }
 
-        uint8_t read(uint16_t address) override;
-        void write(uint16_t address, uint8_t data) override { }; // NROM cannot be written to
+        uint8_t prg_read(uint16_t address) override;
+        void prg_write(uint16_t address, uint8_t data) override { }; // NROM cannot be written to
 
 
 };
