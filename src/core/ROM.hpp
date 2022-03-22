579b5245beb5dfa021e2a6e231685b1f900c72a1
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-03-22 10:43:53 +0000

    added header struct

--- src/core/ROM.hpp
+++ src/core/ROM.hpp
@@ -5,6 +5,18 @@
 #include <vector>
 #include <stdint.h>
 
+struct Header {
+    // contains 16 bytes of data
+    char nes[4]; // NES String followed by MS-DOS end-of-line char $1A
+    uint8_t prg_in_16kb;
+    uint8_t chr_in_8kb;
+    uint8_t flag_6;
+    uint8_t flag_7;
+    uint8_t prg_ram_8kb;
+    uint8_t flag_9;
+    uint8_t flag_10;
+    char zeros[5]; // padding
+};
 class ROM {
 public:
     ROM();
