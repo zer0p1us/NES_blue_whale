f80095bd0d10d6e2620b641cdc79331713c56fa5
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-04-03 18:56:16 +0100

    added LDA into execute()

--- src/core/CPU.cpp
+++ src/core/CPU.cpp
@@ -30,6 +30,54 @@ void CPU::execute(uint8_t instruction){
             #endif
             CLD();
             break;
+        case 0xA9:
+            #ifndef DEBUG
+                std::cout << "LDA immediate" << '\n';
+            #endif
+            LDA(std::bind(&CPU::immediate, this));
+            break;
+        case 0xA5:
+            #ifndef DEBUG
+                std::cout << "LDA zeropage" << '\n';
+            #endif
+            LDA(std::bind(&CPU::zero_page, this));
+            break;
+        case 0xB5:
+            #ifndef DEBUG
+                std::cout << "LDA zeropage + X" << '\n';
+            #endif
+            LDA(std::bind(&CPU::zero_page_x, this));
+            break;
+        case 0xAD:
+            #ifndef DEBUG
+                std::cout << "LDA absolute" << '\n';
+            #endif
+            LDA(std::bind(&CPU::absolute, this));
+            break;
+        case 0xBD:
+            #ifndef DEBUG
+                std::cout << "LDA absolute + X" << '\n';
+            #endif
+            LDA(std::bind(&CPU::absolute_x, this, true));
+            break;
+        case 0xB9:
+            #ifndef DEBUG
+                std::cout << "LDA absolute + Y" << '\n';
+            #endif
+            LDA(std::bind(&CPU::absolute_y, this, true));
+            break;
+        case 0xA1:
+            #ifndef DEBUG
+                std::cout << "LDA indirect + X" << '\n';
+            #endif
+            LDA(std::bind(&CPU::indirect_x, this));
+            break;
+        case 0xB1:
+            #ifndef DEBUG
+                std::cout << "LDA indirect + Y" << '\n';
+            #endif
+            LDA(std::bind(&CPU::indirect_y, this));
+            break;
         case 0xA2:
             #ifdef DEBUG
                 std::cout << "LDX immediate" << '\n';
