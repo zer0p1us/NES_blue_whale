00208fe6ac20bbfe52e8d73fc6c194421af0889c
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-04-09 01:49:47 +0100

    added debug_out function

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,6 +1,13 @@
 #ifndef DEBUG_HEADER
 #define DEBUG_HEADER
 
+#include <string>
+
 #define DEBUG
 
+static void debug_out(std::string debug_message){
+    #ifdef DEBUG
+        std::cout << debug_message << '\n';
+    #endif
+}
 #endif
