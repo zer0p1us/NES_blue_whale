159e284ab6d2585e7c7dfe5fc93c2d39e435201c
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-04-13 22:11:18 +0100

    added comment for virtual functions

--- src/core/Mapper.hpp
+++ src/core/Mapper.hpp
@@ -7,8 +7,8 @@
 class Mapper {
     public:
         Mapper(std::vector<uint8_t> &prg_rom, std::vector<uint8_t> &chr_rom, int mirroring): prg_rom(prg_rom), chr_rom(chr_rom), mirroring(mirroring) { }
-        virtual uint8_t prg_read(uint16_t addr) = 0;
-        virtual void prg_write(uint16_t addr, uint8_t data) = 0;
+        virtual uint8_t prg_read(uint16_t addr) = 0; // zero is required for purely virtual functions
+        virtual void prg_write(uint16_t addr, uint8_t data) = 0; // zero is required for purely virtual functions
 
         virtual uint8_t chr_read(uint16_t addr) { return chr_rom[addr]; }
         virtual void chr_write(uint16_t addr, uint8_t data) { chr_rom[addr] = data; }
