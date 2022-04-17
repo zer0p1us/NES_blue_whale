9f202e52d14e174cf8e508985b02b56cc930149a
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-04-17 18:36:17 +0100

    implemented INX

--- src/core/CPU.hpp
+++ src/core/CPU.hpp
@@ -109,6 +109,7 @@ class CPU {
         void DEY(); // Decrement Index Y by One
         void EOR(std::function<uint16_t()> address); // Exclusive OR Memory with Accumulator
         void INC(std::function<uint16_t()> address); // Increment Memory by one
+        void INX(); // Increment Index X by one
         void JSR(std::function<uint16_t()> address); // Jump and save return address
         void LDA(std::function<uint16_t()> address); // Load Data into Accumulator
         void LDX(std::function<uint16_t()> address); // Load Data into X register
